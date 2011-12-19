0c93d550d032c275d4abce3ab3991557d2a790d4
jackpinto <jackpinto@gmail.com>
2011-12-19 08:56:18 -0200

    Definição dos arquivos de amostra para os testes

---
+++ TestFlatFileImport/BlueprintFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FlatFileImport.Input;
+using FlatFileImport.Process;
+
+namespace TestFlatFileImport
+{
+    public class BlueprintFactory : IBlueprintFactoy
+    {
+        #region IBlueprintFactoy Members
+
+        public Blueprint GetBluePrint(Type type, FileInfo toParse)
+        {
+            return new Blueprint("");
+        }
+
+        #endregion
+    }
+}
---
+++ TestFlatFileImport/TestImport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlatFileImport;
+using FlatFileImport.Process;
+using NUnit.Framework;
+
+namespace TestFlatFileImport
+{   
+    [TestFixture]
+    public class TestImport
+    {
+        [Test]
+        public void TestConstrutor()
+        {
+            IBlueprintFactoy blueprint = new F;
+            var i = new Importer();
+        }
+
+    }
+}
