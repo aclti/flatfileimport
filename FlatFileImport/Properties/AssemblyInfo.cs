99f11030cb3e4f3cc7ed615d80cead84ce8055e5
jackpinto <jackpinto@gmail.com>
2012-05-04 18:14:37 -0300

    Atualização dos dados do AssmblyInfo para a criação da tag

--- FlatFileImport/Properties/AssemblyInfo.cs
+++ FlatFileImport/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FlatFileImport")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("ACL TI")]
 [assembly: AssemblyProduct("FlatFileImport")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2011")]
+[assembly: AssemblyCopyright("Copyright © Benefix 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: NeutralResourcesLanguageAttribute("pt-BR")]
